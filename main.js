bda2e0ddd331ce1496617b7615e25317f6e50027
linganand-04 <linganandb2004@gmail.com>
2025-01-03 21:33:39 +0530

    Backup file succesfully

--- main.js
+++ main.js
@@ -13,215 +13,215 @@ const fundamentals = document.querySelector("#funda");
 const english = document.querySelector("#engTyp");
 
 fetch("./demo.json")
-  .then((res) => res.json())
-  .then((data) => {
-    const intern = data[0].intership;
-    const web = data[0].webdevelopment;
-    const funda = data[0].fundamentals;
-    const eng = data[0].english;
+.then((res) => res.json())
+.then((data) => {
+const intern = data[0].intership;
+const web = data[0].webdevelopment;
+const funda = data[0].fundamentals;
+const eng = data[0].english;
 
-    // **** INTERNSHIP **** //
+// **** INTERNSHIP **** //
 
-    intern.forEach((post) => {
-      profile.insertAdjacentHTML(
-        "beforeend",
-        `<div class="new-profile">
+intern.forEach((post) => {
+profile.insertAdjacentHTML(
+"beforeend",
+`<div class="new-profile">
 <div class="profile-bg">
-  <img
-    class="profile-banner"
-    src="${post.profile_banner}"
-    alt="Coding Img"
-  />
-  <img
-    class="student-profile"
-    src="${post.student_profile}"
-    alt="Paramanand"
-  />
+ <img
+   class="profile-banner"
+   src="${post.profile_banner}"
+   alt="Coding Img"
+ />
+ <img
+   class="student-profile"
+   src="${post.student_profile}"
+   alt="Paramanand"
+ />
 </div>
 <h3 class="heading-secondary">${post.heading_secondary}</h3>
 <div class="content-box">
-  <p class="skills">My skills</p>
-  <p class="profile-skills">
-    ${post.profile_skills}
-  </p>
-  <p class="profile-bio">Bio :</p>
-  <p class="profile-content">
-    ${post.profile_content}
-  </p>
+ <p class="skills">My skills</p>
+ <p class="profile-skills">
+   ${post.profile_skills}
+ </p>
+ <p class="profile-bio">Bio :</p>
+ <p class="profile-content">
+   ${post.profile_content}
+ </p>
 </div>
 <div class="profile-link">
-  <a
-    class="btn btn-1"
-    target="_blank"
-    href="${post.profile_view}"
-    >View Profile</a
-  >
-  <a
-    class="btn btn-2"
-    target="_blank"
-    href="${post.profile_projects}"
-    >View Projects</a
-  >
+ <a
+   class="btn btn-1"
+   target="_blank"
+   href="${post.profile_view}"
+   >View Profile</a
+ >
+ <a
+   class="btn btn-2"
+   target="_blank"
+   href="${post.profile_projects}"
+   >View Projects</a
+ >
 </div>
 </div>`
-      );
-    });
+);
+});
 
-    // **** WEB DEVELOPMENT **** //
+// **** WEB DEVELOPMENT **** //
 
-    web.forEach((post) => {
-      webdevelopment.insertAdjacentHTML(
-        "beforeend",
-        `<div class="new-profile">
-  <div class="profile-bg">
+web.forEach((post) => {
+webdevelopment.insertAdjacentHTML(
+"beforeend",
+`<div class="new-profile">
+ <div class="profile-bg">
+   <img
+      class="profile-banner"
+      src="${post.profile_banner}"
+      alt="Coding Img"
+    />
     <img
-       class="profile-banner"
-       src="${post.profile_banner}"
-       alt="Coding Img"
-     />
-     <img
-       class="student-profile"
-       src="${post.student_profile}"
-       alt="Paramanand"
-     />
-   </div>
-   <h3 class="heading-secondary">${post.heading_secondary}</h3>
-   <div class="content-box">
-     <p class="skills">My skills</p>
-     <p class="profile-skills">
-       ${post.profile_skills}
-     </p>
-     <p class="profile-bio">Bio :</p>
-     <p class="profile-content">
-      ${post.profile_content}
-     </p>
-   </div>
-   <div class="profile-link">
-     <a
-       class="btn btn-1"
-       target="_blank"
-       href="${post.profile_view}"
-       >View Profile</a
-     >
-     <a
-       class="btn btn-2"
-       target="_blank"
-       href="${post.profile_projects}"
-       >View Projects</a
-     >
-   </div>
-   </div>`
-      );
-    });
+      class="student-profile"
+      src="${post.student_profile}"
+      alt="Paramanand"
+    />
+  </div>
+  <h3 class="heading-secondary">${post.heading_secondary}</h3>
+  <div class="content-box">
+    <p class="skills">My skills</p>
+    <p class="profile-skills">
+      ${post.profile_skills}
+    </p>
+    <p class="profile-bio">Bio :</p>
+    <p class="profile-content">
+     ${post.profile_content}
+    </p>
+  </div>
+  <div class="profile-link">
+    <a
+      class="btn btn-1"
+      target="_blank"
+      href="${post.profile_view}"
+      >View Profile</a
+    >
+    <a
+      class="btn btn-2"
+      target="_blank"
+      href="${post.profile_projects}"
+      >View Projects</a
+    >
+  </div>
+  </div>`
+);
+});
 
-    // **** COMPUTER FUNDAMENTAL **** //
+// **** COMPUTER FUNDAMENTAL **** //
 
-    funda.forEach((post) => {
-      fundamentals.insertAdjacentHTML(
-        "beforeend",
-        `<div class="new-profile">
-      <div class="profile-bg">
+funda.forEach((post) => {
+fundamentals.insertAdjacentHTML(
+"beforeend",
+`<div class="new-profile">
+     <div class="profile-bg">
+       <img
+          class="profile-banner"
+          src="${post.profile_banner}"
+          alt="Coding Img"
+        />
         <img
-           class="profile-banner"
-           src="${post.profile_banner}"
-           alt="Coding Img"
-         />
-         <img
-           class="student-profile"
-           src="${post.student_profile}"
-           alt="Paramanand"
-         />
-       </div>
-       <h3 class="heading-secondary">${post.heading_secondary}</h3>
-       <div class="content-box">
-         <p class="skills">My skills</p>
-         <p class="profile-skills">
-           ${post.profile_skills}
-         </p>
-         <p class="profile-bio">Bio :</p>
-         <p class="profile-content">
-          ${post.profile_content}
-         </p>
-       </div>
-       <div class="profile-link">
-         <a
-           class="btn btn-1"
-           target="_blank"
-           href="${post.profile_view}"
-           >View Profile</a
-         >
-         <a
-           class="btn btn-2"
-           target="_blank"
-           href="${post.profile_projects}"
-           >View Projects</a
-         >
-       </div>
-       </div>`
-      );
-    });
+          class="student-profile"
+          src="${post.student_profile}"
+          alt="Paramanand"
+        />
+      </div>
+      <h3 class="heading-secondary">${post.heading_secondary}</h3>
+      <div class="content-box">
+        <p class="skills">My skills</p>
+        <p class="profile-skills">
+          ${post.profile_skills}
+        </p>
+        <p class="profile-bio">Bio :</p>
+        <p class="profile-content">
+         ${post.profile_content}
+        </p>
+      </div>
+      <div class="profile-link">
+        <a
+          class="btn btn-1"
+          target="_blank"
+          href="${post.profile_view}"
+          >View Profile</a
+        >
+        <a
+          class="btn btn-2"
+          target="_blank"
+          href="${post.profile_projects}"
+          >View Projects</a
+        >
+      </div>
+      </div>`
+);
+});
 
-    // ***** SPOKEN ENGLISH ***** //
+// ***** SPOKEN ENGLISH ***** //
 
-    eng.forEach((post) => {
-      english.insertAdjacentHTML(
-        "beforeend",
-        `<div class="new-profile">
-          <div class="profile-bg">
+eng.forEach((post) => {
+english.insertAdjacentHTML(
+"beforeend",
+`<div class="new-profile">
+         <div class="profile-bg">
+           <img
+              class="profile-banner"
+              src="${post.profile_banner}"
+              alt="Coding Img"
+            />
             <img
-               class="profile-banner"
-               src="${post.profile_banner}"
-               alt="Coding Img"
-             />
-             <img
-               class="student-profile"
-               src="${post.student_profile}"
-               alt="Paramanand"
-             />
-           </div>
-           <h3 class="heading-secondary">${post.heading_secondary}</h3>
-           <div class="content-box">
-             <p class="skills">My skills</p>
-             <p class="profile-skills">
-               ${post.profile_skills}
-             </p>
-             <p class="profile-bio">Bio :</p>
-             <p class="profile-content">
-              ${post.profile_content}
-             </p>
-           </div>
-           <div class="profile-link">
-             <a
-               class="btn btn-1"
-               target="_blank"
-               href="${post.profile_view}"
-               >View Profile</a
-             >
-             <a
-               class="btn btn-2"
-               target="_blank"
-               href="${post.profile_projects}"
-               >View Projects</a
-             >
-           </div>
-           </div>`
-      );
-    });
-  });
+              class="student-profile"
+              src="${post.student_profile}"
+              alt="Paramanand"
+            />
+          </div>
+          <h3 class="heading-secondary">${post.heading_secondary}</h3>
+          <div class="content-box">
+            <p class="skills">My skills</p>
+            <p class="profile-skills">
+              ${post.profile_skills}
+            </p>
+            <p class="profile-bio">Bio :</p>
+            <p class="profile-content">
+             ${post.profile_content}
+            </p>
+          </div>
+          <div class="profile-link">
+            <a
+              class="btn btn-1"
+              target="_blank"
+              href="${post.profile_view}"
+              >View Profile</a
+            >
+            <a
+              class="btn btn-2"
+              target="_blank"
+              href="${post.profile_projects}"
+              >View Projects</a
+            >
+          </div>
+          </div>`
+);
+});
+});
 
 
 // *************** Sticky For Profile Bio ******************** //
 
 function applyStickyStyle() {
-  const bio = document.querySelectorAll(".profile-bio");
+const bio = document.querySelectorAll(".profile-bio");
 
-  bio.forEach((el) => {
-    const style = window.getComputedStyle(el);
-    if (style.position === "sticky") {
-      el.style.color = "black";
-      el.style.backgroundColor = "white";
-    }
-  });
+bio.forEach((el) => {
+const style = window.getComputedStyle(el);
+if (style.position === "sticky") {
+el.style.color = "black";
+el.style.backgroundColor = "white";
+}
+});
 }
 
 // Call the function for specific elements
@@ -236,11 +236,11 @@ const batchYear = document.querySelector(".batch-year");
 const body = document.querySelector("#body")
 
 batch.addEventListener("click", () => {
-  batchYear.classList.toggle("batch-child");
+batchYear.classList.toggle("batch-child");
 });
 
 body.addEventListener("click", () => {
-  batchYear.classList.remove("batch-child");
+batchYear.classList.remove("batch-child");
 },true);
 
 // ************************ //
@@ -248,27 +248,27 @@ body.addEventListener("click", () => {
 //change navbar stcccle//
 
 window.addEventListener("scroll", () => {
-  document
-    .querySelector(".main-nav")
-    .classList.toggle("window_scroll", window.scrollY > 0);
+document
+.querySelector(".main-nav")
+.classList.toggle("window_scroll", window.scrollY > 0);
 });
 
 window.addEventListener("scroll", () => {
-  document
-    .querySelector(".container-navbar-2")
-    .classList.toggle("navbar-2-scroll", window.scrollY > 0);
+document
+.querySelector(".container-navbar-2")
+.classList.toggle("navbar-2-scroll", window.scrollY > 0);
 });
 
 window.addEventListener("scroll", () => {
-  document
-    .querySelector(".course")
-    .classList.toggle("course-scroll", window.scrollY > 0);
+document
+.querySelector(".course")
+.classList.toggle("course-scroll", window.scrollY > 0);
 });
 
 window.addEventListener("scroll", () => {
-  document.querySelectorAll(".list-items").forEach((item) => {
-    item.classList.toggle("list-items-scroll", window.scrollY > 0);
-  });
+document.querySelectorAll(".list-items").forEach((item) => {
+item.classList.toggle("list-items-scroll", window.scrollY > 0);
+});
 });
 
 window.addEventListener("scroll", () => {
@@ -283,60 +283,35 @@ window.addEventListener("scroll", () => {
 
 const faqs = document.querySelectorAll(".faq");
 faqs.forEach((faq) => {
-  faq.addEventListener("click", () => {
-    faq.classList.toggle("open");
-    //change icon
-    const icon = faq.querySelector(".faq__icon i");
-    if (icon.className === "uil uil-plus") {
-      icon.className = "uil uil-minus";
-    } else {
-      icon.className = "uil uil-plus";
-    }
-  });
+faq.addEventListener("click", () => {
+faq.classList.toggle("open");
+//change icon
+const icon = faq.querySelector(".faq__icon i");
+if (icon.className === "uil uil-plus") {
+icon.className = "uil uil-minus";
+} else {
+icon.className = "uil uil-plus";
+}
+});
 });
 //===========show /hide nav menu==========//
-
 const menu = document.querySelector(".nav__menu ");
 const menubtn = document.querySelector("#open-menu-btn ");
 const closebtn = document.querySelector("#close-menu-btn ");
 
-
 menubtn.addEventListener("click", () => {
-  menu.style.display = "flex";
-  menu.style.flexDirection = "column";
-  closebtn.style.display = "inline-block";
-  menubtn.style.display = "none";
+menu.style.display = "flex";
+menu.style.flexDirection = "column";
+closebtn.style.display = "inline-block";
+menubtn.style.display = "none";
 });
 
 //=============close nav menu===========//
 
 const closenav = () => {
-  menu.style.display = "none";
-  closebtn.style.display = "none";
-  menubtn.style.display = "inline-block";
+menu.style.display = "none";
+closebtn.style.display = "none";
+menubtn.style.display = "inline-block";
 };
 closebtn.addEventListener("click", closenav);
 
-
-<<<<<<< HEAD
-// // Select all instances of the menu buttons and close buttons
-// const menus = document.querySelectorAll('.nav');
-
-// menus.forEach(menu => {
-//   const menubtn = menu.querySelector('.open-menu-btn');
-//   const closebtn = menu.querySelector('.close-menu-btn');
-//   const menuContent = menu.querySelector('.nav__menu');
-
-//   // When the open button is clicked, show the menu
-//   menubtn.addEventListener('click', () => {
-//     menuContent.classList.add('open');
-//   });
-
-//   // When the close button is clicked, hide the menu
-//   closebtn.addEventListener('click', () => {
-//     menuContent.classList.remove('open');
-//   });
-// });
-
-=======
->>>>>>> 04d0794707fd7f8069c2d016fc4d2d9efddfa901
