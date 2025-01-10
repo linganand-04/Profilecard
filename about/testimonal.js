019fbdcb4dc74c7cd78d1740d48806fc3f616663
linganand-04 <linganandb2004@gmail.com>
2025-01-10 22:17:46 +0530

    about us page inprocess

--- about/testimonal.js
+++ about/testimonal.js
@@ -15,17 +15,18 @@ window.addEventListener("scroll", () => {
   });
   
   function showInfo(item) {
-    showDiv.querySelector(".show-img img").src = item.querySelector(
-      ".client-thumbnail img"
-    ).src;
-    showDiv.querySelector(".show-name").innerHTML =
-      item.querySelector(".client-name").innerHTML;
-    showDiv.querySelector(".show-designation").innerHTML = item.querySelector(
-      ".client-designation"
-    ).innerHTML;
-    showDiv.querySelector(".show-description").innerHTML = item.querySelector(
-      ".client-description"
-    ).innerHTML;
+    showDiv.querySelector(".show-img img").src = item.querySelector(".client-thumbnail img").src;
+    showDiv.querySelector(".show-name").innerHTML = item.querySelector(".client-name").innerHTML;
+    showDiv.querySelector(".show-designation").innerHTML = item.querySelector(".client-designation").innerHTML;
+    showDiv.querySelector(".show-description").innerHTML = item.querySelector(".client-description").innerHTML;
+    // Temp //
+    showDiv.querySelector(".client-heading one").innerHTML = item.querySelector(".client-heading").innerHTML;
+    showDiv.querySelector(".client-education").innerHTML = item.querySelector(".client-education").innerHTML;
+    showDiv.querySelector(".client-heading two").innerHTML = item.querySelector(".client-heading").innerHTML;    
+    showDiv.querySelector(".client-experience").innerHTML = item.querySelector(".client-experience").innerHTML;    
+    showDiv.querySelector(".client-heading three").innerHTML = item.querySelector(".client-heading").innerHTML;
+    showDiv.querySelector(".client-training").innerHTML = item.querySelector(".client-training").innerHTML;
+
     setActiveTab(item);
   }
   
